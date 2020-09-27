2ce1520b581eb16086aa5f0932ed1ae3ac14a601
adamazmil <muhd.adam1@gmail.com>
2020-09-27 14:08:37 -0500

    working canvas

--- CS559_H2/H2.js
+++ CS559_H2/H2.js
@@ -1,5 +1,24 @@
-/**
- * Created by Adam
- * Based on Quadcopter demo 
- */
-"use strict ";
+function setup() { "use strict";
+  var canvas = document.getElementById('myCanvas');
+  function draw() {
+    var context = canvas.getContext('2d');
+    canvas.width = canvas.width;
+
+    function body(){
+        context.strokeStyle = "black";
+        context.beginPath();
+        context.arc(640,360,100,0, Math.PI*2);
+        context.fillStyle = "black";
+        context.fill();
+        context.stroke();
+    }
+
+    body();
+    context.restore();
+  }
+
+  draw();
+}
+window.onload = setup;
+
+
