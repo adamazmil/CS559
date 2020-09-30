cc3be70a1f37d73d9f515e460ebc57fb33197762
adamazmil <muhd.adam1@gmail.com>
2020-09-30 16:45:10 -0500

    completed bare minimum

--- CS559_H2/H2.js
+++ CS559_H2/H2.js
@@ -1,12 +1,15 @@
 function setup() { "use strict";
   var canvas = document.getElementById('myCanvas');
   var vert = document.getElementById  ('vertical');
+  var mod = document.getElementById ('move');
+  mod.value = 0;
   vert.value = 0;
   
 
   function draw() {
     var context = canvas.getContext('2d');
     canvas.width = canvas.width;
+    let theta = mod.value*(Math.PI/180);
     let v = vert.value;
     let angle1 = -40*(Math.PI/180);
     let angle2 = 85*(Math.PI/180);
@@ -43,7 +46,6 @@ function setup() { "use strict";
       context.stroke();
     }
     function drawSpider(){
-      console.log(v);
       context.translate (500, 360+(+v));
       context.save();
       web();
@@ -58,7 +60,7 @@ function setup() { "use strict";
       body(50);
       context.restore();
       context.save();
-      context.rotate(angle1);
+      context.rotate(angle1 + theta);
       context.scale(2,1);
       //back legs
       legs();
@@ -73,7 +75,7 @@ function setup() { "use strict";
       context.restore();
       context.save();
       context.scale(2,1);
-      context.rotate(angle5);
+      context.rotate(angle5 + theta);
       legs();
       context.translate(100,0);
       context.scale(1/2,1);
@@ -83,7 +85,7 @@ function setup() { "use strict";
       context.restore();
       context.save();
       //2nd legs
-      context.rotate(angle4);
+      context.rotate(angle4 - theta);
       context.scale(1.5,1);
       legs();
       context.translate(100,0);
@@ -94,7 +96,7 @@ function setup() { "use strict";
       context.save();
       //1st legs
       context.translate(0,70);
-      context.rotate(angle4);
+      context.rotate(angle4 - theta);
       legs();
       context.translate(90,0);
       context.rotate(angle3);
@@ -106,7 +108,7 @@ function setup() { "use strict";
       //lst legs
       context.translate(0,70);
       context.scale(-1,1);
-      context.rotate(angle4);
+      context.rotate(angle4 - theta );
       legs();
       context.translate(90,0);
       context.rotate(angle3);
@@ -114,9 +116,9 @@ function setup() { "use strict";
       legs();
       context.restore();
       context.save();
-      context.rotate(-angle1);
-      context.scale(-2,1);
       //back legs
+      context.rotate(-angle1 - theta);
+      context.scale(-2,1);
       legs();
       context.save();
       context.translate(100,0);
@@ -129,7 +131,7 @@ function setup() { "use strict";
       context.save();
       //middle legs
       context.scale(-2,1);
-      context.rotate(angle5);
+      context.rotate(angle5 + theta);
       legs();
       context.translate(100,0);
       context.scale(1/2,1);
@@ -139,7 +141,7 @@ function setup() { "use strict";
       context.restore();
       context.save();
       //2nd legs
-      context.rotate(-angle4);
+      context.rotate(-angle4 + theta);
       context.scale(-1.5,1);
       legs();
       context.translate(100,0);
@@ -156,6 +158,7 @@ function setup() { "use strict";
     
   }
   vert.addEventListener("input",draw);
+  mod.addEventListener("input",draw);
   draw();
 }
 window.onload = setup;
